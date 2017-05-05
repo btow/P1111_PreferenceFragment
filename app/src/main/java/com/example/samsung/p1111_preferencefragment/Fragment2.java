080a2a57ff90c4297417518fa1dff1eb1445c5ee
btow <btow@yandex.ru>
2017-05-05 15:58:40 +0500

    It's OK

---
+++ app/src/main/java/com/example/samsung/p1111_preferencefragment/Fragment2.java
@@ -0,0 +1,18 @@
+package com.example.samsung.p1111_preferencefragment;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by samsung on 05.05.2017.
+ */
+
+public class Fragment2 extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref2);
+    }
+}
--- app/src/main/java/com/example/samsung/p1111_preferencefragment/MainActivity.java
+++ app/src/main/java/com/example/samsung/p1111_preferencefragment/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.samsung.p1111_preferencefragment;
 
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.List;
+
+/**
+ * Simple preferens acnbvity
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(android.R.id.content, new Fragment1())
                 .commit();
+    }
+}*/
+
+public class MainActivity extends PreferenceActivity {
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        super.onBuildHeaders(target);
+        loadHeadersFromResource(R.xml.pref_head, target);
     }
 }
