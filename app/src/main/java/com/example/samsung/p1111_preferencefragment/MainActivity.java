e7e8fcf9f9bd93acb9a69ca42d586cecd142ceee
btow <btow@yandex.ru>
2017-05-05 15:33:30 +0500

    Initial commit

---
+++ app/src/main/java/com/example/samsung/p1111_preferencefragment/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.samsung.p1111_preferencefragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new Fragment1())
+                .commit();
+    }
+}
